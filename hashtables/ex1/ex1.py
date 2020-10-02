aaf08ec1950ff914d2ec048e9e23f01baefc5199
worldwidekatie <katie.evankodouglas@gmail.com>
2020-10-02 10:42:50 -0600

    passed ex1 tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,15 +3,10 @@ def get_indices_of_item_weights(weights, length, limit):
     cache = {}
     for i in range(length-1):
         for x in range(len(weights[i:])):
-            print(x)
-            # #x+1
-            # #if i != x+1:
-            # print(weights[i], weights[i+x+1])
-            # #if cache[weights[i] + ]
+            if x != 0:
+                cache[weights[i] + weights[i+x]] = (i+x, i)
 
-    return None
-
-weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
-print(get_indices_of_item_weights(weights_4, 9, 7))
-# self.assertTrue(answer_4[0] == 6)
-# self.assertTrue(answer_4[1] == 2)
+    if limit in cache:
+        return cache[limit]
+    else:
+        return None
