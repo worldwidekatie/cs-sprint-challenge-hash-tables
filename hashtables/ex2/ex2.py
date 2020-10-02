18cbd550152ade5e6832363bfdeac13daa075acf
worldwidekatie <katie.evankodouglas@gmail.com>
2020-10-02 09:18:17 -0600

    passed ex2 tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,12 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
-class Ticket:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
-
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    for i in tickets:
+        cache[i.source] = i.destination
 
+    route = []
+    current_ticket = cache["NONE"]
+    for i in range(0, length):
+        route.append(current_ticket)
+        current_ticket = cache[current_ticket]
     return route
